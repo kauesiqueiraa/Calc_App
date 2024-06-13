838bcaea50bbed578c15671f82b560d73a199704
kauesiqueiraa <kauesiqueira_@hotmail.com>
2024-06-13 09:12:28 -0300

    feat: create base layout calc

--- lib/main.dart
+++ lib/main.dart
@@ -10,60 +10,71 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      title: 'Calculadora App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child:Text('Calculadora'),
+          ),
+        ),
+        body: const Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text('Numero', style: TextStyle(fontSize: 72),),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('AC', style: TextStyle(fontSize: 48),),
+                Text(''),
+                Text(''),
+                Text('<X', style: TextStyle(fontSize: 48),),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('7', style: TextStyle(fontSize: 48),),
+                Text('8', style: TextStyle(fontSize: 48),),
+                Text('9', style: TextStyle(fontSize: 48),),
+                Text('/', style: TextStyle(fontSize: 48),),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('4', style: TextStyle(fontSize: 48),),
+                Text('5', style: TextStyle(fontSize: 48),),
+                Text('6', style: TextStyle(fontSize: 48),),
+                Text('X', style: TextStyle(fontSize: 48),),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('1', style: TextStyle(fontSize: 48),),
+                Text('2', style: TextStyle(fontSize: 48),),
+                Text('3', style: TextStyle(fontSize: 48),),
+                Text('-', style: TextStyle(fontSize: 48),),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('0', style: TextStyle(fontSize: 48),),
+                Text(',', style: TextStyle(fontSize: 48),),
+                Text('=', style: TextStyle(fontSize: 48),),
+                Text('+', style: TextStyle(fontSize: 48),),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
